69b3008f06ec1ded3846bce176bf25d5fad83dd1
DHANESWAR BHAKAT <dhaneswarbhakat2000@gmail.com>
2024-04-20 10:46:43 +0530

    Add necessary files for Linux, iOS, macOS, and Windows platforms

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+const url = 'http://192.168.53.203:4000/';
+final registration = url + "register";
+final login = url + 'login';
+final addtodo = url + 'createToDo';
+final getToDoList = url + 'getUserTodoList';
+final deleteTodo = url + 'deleteTodo';
+
